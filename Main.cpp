228312acb807d75c552cdf5290178455171ca177
Douglas Maitelli <dougmaitelli@gmail.com>
2017-07-30 20:05:10 +0200

    GLFW

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,9 @@
-#include <Windows.h>
-#include "include/GL/glut.h"
 #include <iostream>
-#include <fstream>
+#include <algorithm>
 #include <math.h>
+
+#include "include/GLFW/glfw3.h"
+
 #include "PTMReader.h"
 #include "GameObject.h"
 #include "Timer.h"
@@ -10,42 +11,35 @@
 
 using namespace std;
 
-bool* keyStates = new bool[256];
-bool movendo = false;
+bool moving = false;
 
 int alturaPulo = 100;
-int velocidade = 10;
-int velocidadePulo = 5;
+int velocity = 10;
+int jumpVelocity = 5;
 
 vector<Layer*> layers(5);
 
-Image *personagemSpr;
-Image *inimigoSpr;
-Image *nuvem, *montanha, *chao, *plataforma, *plataforma2;
+Image *playerSpr;
+Image *enemySpr;
+Image *clouds, *mountain, *ground, *platform, *backPlatform;
 
-Image *scene, *backup;
-int *zbuffer, *zbuffer2;
+Image *scene;
+int *zbuffer;
 
 Timer timer;
 
-GameObject *personagem;
+GameObject *player;
 int frameCounter = 0;
 
-vector<GameObject*> inimigos;
+vector<GameObject*> enemies;
 
-void preencheZBuffer(int *zbuffer, int z) {
+void clearZBuffer(int *zbuffer) {
 	for (int y = 0; y < scene->getHeight(); y++)
 		for (int x = 0; x < scene->getWidth(); x++)
-			zbuffer[x + y*scene->getWidth()] = z;
+			zbuffer[x + y*scene->getWidth()] = 0;
 }
 
-void copiaZBuffer(int *zbuffer, int *zbuffer2) {
-	for (int y = 0; y < scene->getHeight(); y++)
-		for (int x = 0; x < scene->getWidth(); x++)
-			zbuffer2[x + y*scene->getWidth()] = zbuffer[x + y*scene->getWidth()];
-}
-
-int getChaoY(Layer* layer, int x, int layerOffset) {
+int getGroundY(Layer* layer, int x, int layerOffset) {
 	int chaoY = 0;
 
 	bool inicioChao = false;
@@ -68,42 +62,39 @@ int getChaoY(Layer* layer, int x, int layerOffset) {
 	return chaoY;
 }
 
-int getChaoY(int x, int y) {
-	int chaoYC = getChaoY(layers[4], x, 0);
-	int chaoYP1 = getChaoY(layers[3], x, 50);
-	int chaoYP2 = getChaoY(layers[2], x, 50);
+int getGroundY(int x, int y) {
+	int groundYG = getGroundY(layers[4], x, 0);
+	int groundYP = getGroundY(layers[3], x, 50);
+	int groundYBP = getGroundY(layers[2], x, 50);
 	
-	int chaoY = 0;
-	if (chaoYC <= y + 7) {
-		chaoY = chaoYC;
+	int groundY = 0;
+	if (groundYG <= y + 7) {
+		groundY = groundYG;
 	}
-	if (chaoYC < chaoYP1 && chaoYP1 <= y + 5) {
-		chaoY = chaoYP1;
+	if (groundYG < groundYP && groundYP <= y + 5) {
+		groundY = groundYP;
 	}
-	if (chaoYP1 < chaoYP2 && chaoYP2 <= y + 5) {
-		chaoY = chaoYP2;
+	if (groundYP < groundYBP && groundYBP <= y + 5) {
+		groundY = groundYBP;
 	}
 	
-	return chaoY;
+	return groundY;
 }
 
 void sceneComposition(void) {
 	(*scene).plotLayerRepeat(layers[0]->getImagem(), 0, 0, layers[0]->getPosX(), 0);
-	preencheZBuffer(zbuffer, 0);
+	clearZBuffer(zbuffer);
 
 	(*scene).plotLayerZBufferRepeat(layers[1]->getImagem(), 0, 50, layers[1]->getPosX(), 0, zbuffer, 1);
 	(*scene).plotLayerZBufferRepeat(layers[2]->getImagem(), 0, 50, layers[2]->getPosX(), 0, zbuffer, 2);
 	(*scene).plotLayerZBufferRepeat(layers[3]->getImagem(), 0, 50, layers[3]->getPosX(), 0, zbuffer, 3);
 	(*scene).plotLayerZBufferRepeat(layers[4]->getImagem(), 0, 0, layers[4]->getPosX(), 0, zbuffer, 4);
 
-	(*scene).plot(personagem->getCurrentFrame(), personagem->getX(), personagem->getY());
+	(*scene).plot(player->getCurrentFrame(), player->getX(), player->getY());
 
-	for (int i = 0; i < inimigos.size(); i++) {
-		(*scene).plot(inimigos[i]->getCurrentFrame(), inimigos[i]->getX(), inimigos[i]->getY());
+	for (int i = 0; i < enemies.size(); i++) {
+		(*scene).plot(enemies[i]->getCurrentFrame(), enemies[i]->getX(), enemies[i]->getY());
 	}
-
-	(*scene).copy(backup);
-	copiaZBuffer(zbuffer, zbuffer2);
 }
 
 void scroll(bool plus, int valor) {
@@ -112,87 +103,80 @@ void scroll(bool plus, int valor) {
 	}
 }
 
-int calcWaitingTime(int fps, int elapsedTime) {
-	int wt = (1000 / fps) - elapsedTime;
-	return wt;
-}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (action == GLFW_PRESS) {
+		if(key == GLFW_KEY_ESCAPE)
+			glfwSetWindowShouldClose(window, GLFW_TRUE);
 
-void keyPressed(unsigned char key, int x, int y) {
-	if (key == 'q') {
-		return exit(0);
+		if (key == GLFW_KEY_SPACE && player->getEstaPulando() == 0) {
+			player->setEstaPulando(alturaPulo);
+		}
+		else if (key == GLFW_KEY_S) {
+			player->setY(player->getY() - 15);
+		}
+
+		if (key == GLFW_KEY_A) {
+			moving = true;
+			player->setDirection(-1);
+		}
+		else if (key == GLFW_KEY_D) {
+			moving = true;
+			player->setDirection(1);
+		}
 	}
-
-	keyStates[key] = true;
-
-	if (keyStates[32] && personagem->getEstaPulando() == 0) {
-		personagem->setEstaPulando(alturaPulo);
-	}
-	else if (keyStates['s']) {
-		personagem->setY(personagem->getY() - 15);
-	}
-}
-
-void keyUp(unsigned char key, int x, int y) {
-	keyStates[key] = false;
-}
-
-void keyboardHoldActions() {
-	if (keyStates['a']) {
-		movendo = true;
-		personagem->setDirection(-1);
-	}
-	else if (keyStates['d']) {
-		movendo = true;
-		personagem->setDirection(1);
+	else if (action == GLFW_RELEASE) {
+		if (key == GLFW_KEY_A || key == GLFW_KEY_D) {
+			moving = false;
+		}
 	}
 }
 
 int posSinceLastEnemy = 0;
-void geraInimigos() {
-	if (layers[4]->getPosX() - posSinceLastEnemy > 100 && inimigos.size() < 5) {
+void generateEnemies() {
+	if (layers[4]->getPosX() - posSinceLastEnemy > 100 && enemies.size() < 5) {
 		srand(time(NULL));
 		if (rand() % 100 > 10) {
 			return;
 		}
 
-		GameObject* inimigo = new GameObject();
-		inimigo->init(PTMReader::read("Graphics/Personagem.ptm"));
-		inimigo->setX(600);
-		inimigo->setY(300);
-		inimigo->setDirection(-1);
+		GameObject* enemy = new GameObject();
+		enemy->init(PTMReader::read("Graphics/Personagem.ptm"));
+		enemy->setX(600);
+		enemy->setY(300);
+		enemy->setDirection(-1);
 
-		inimigos.push_back(inimigo);
+		enemies.push_back(enemy);
 
 		posSinceLastEnemy = (int)layers[4]->getPosX();
 	}
 }
 
-void moveInimigos() {
-	for (int i = 0; i < inimigos.size(); i++) {
-		inimigos[i]->setX(inimigos[i]->getX() - 2);
+void moveEnemies() {
+	for (int i = 0; i < enemies.size(); i++) {
+		enemies[i]->setX(enemies[i]->getX() - 2);
 		if (frameCounter >= 5) {
-			inimigos[i]->incCurrentFrame();
+			enemies[i]->incCurrentFrame();
 		}
 	}
 }
 
-void calculaGravidade(GameObject* obj) {
+void calcGravity(GameObject* obj) {
 	if (obj->getEstaPulando() > 0) {
-		obj->setEstaPulando(personagem->getEstaPulando() - velocidadePulo);
-		obj->setY(personagem->getY() + velocidadePulo);
+		obj->setEstaPulando(player->getEstaPulando() - jumpVelocity);
+		obj->setY(player->getY() + jumpVelocity);
 	}
 	else if (obj->getY() > 0) {
-		int chaoYS = getChaoY(obj->getX() + 10, obj->getY());
-		int chaoYE = getChaoY(obj->getX() + 60, obj->getY());
+		int chaoYS = getGroundY(obj->getX() + 10, obj->getY());
+		int chaoYE = getGroundY(obj->getX() + 60, obj->getY());
 
-		int maxChao = max(chaoYS, chaoYE);
+		int maxGround = max(chaoYS, chaoYE);
 
-		if (obj->getY() > maxChao) {
-			obj->setY(obj->getY() - velocidadePulo);
+		if (obj->getY() > maxGround) {
+			obj->setY(obj->getY() - jumpVelocity);
 		}
 
-		if (obj->getY() < maxChao) {
-			obj->setY(maxChao);
+		if (obj->getY() < maxGround) {
+			obj->setY(maxGround);
 		}
 
 		if (obj->getY() < 50) {
@@ -201,80 +185,51 @@ void calculaGravidade(GameObject* obj) {
 	}
 }
 
-void update(int value) {
-	timer.start();
+void update() {
+	int value = 0;
 
-	keyboardHoldActions();
-
-	int valor = 0;
-
-	if (movendo) {
-		valor = personagem->getDirection() * velocidade;
+	if (moving) {
+		value = player->getDirection() * velocity;
 	}
 
-	if (movendo && frameCounter >= 5) {
-		personagem->incCurrentFrame();
+	if (moving && frameCounter >= 5) {
+		player->incCurrentFrame();
 		frameCounter = 0;
 	}
 	frameCounter++;
 
-	calculaGravidade(personagem);
-	for (int i = 0; i < inimigos.size(); i++) {
-		calculaGravidade(inimigos[i]);
+	calcGravity(player);
+	for (int i = 0; i < enemies.size(); i++) {
+		calcGravity(enemies[i]);
 	}
 
-	scroll(false, valor);
+	scroll(false, value);
 
-	geraInimigos();
-	moveInimigos();
+	generateEnemies();
+	moveEnemies();
 
 	sceneComposition();
-	glutPostRedisplay();
-
-	timer.finish();
-
-	movendo = false;
-
-	int waitingTime = calcWaitingTime(60, timer.getElapsedTimeMs());
-	glutTimerFunc(waitingTime, update, value);
-}
-
-void display(void) {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glLoadIdentity();
-
-	GLint iViewport[4];
-	glGetIntegerv(GL_VIEWPORT, iViewport);
-	glPixelZoom((float) iViewport[2] / (float) (*scene).getWidth(), (float) iViewport[3] / (float) (*scene).getHeight());
-
-	glDrawPixels((*scene).getWidth(), (*scene).getHeight(), GL_BGRA_EXT, GL_UNSIGNED_BYTE, (*scene).getPixels());
-	glutSwapBuffers();
-
-	glFlush();
 }
 
 void initJogo(void) {
 	scene = new Image(640, 480);
-	backup = new Image(640, 480);
 
 	zbuffer = new int[scene->getWidth()*scene->getHeight()];
-	zbuffer2 = new int[scene->getWidth()*scene->getHeight()];
 
-	nuvem = PTMReader::read("Graphics/Nuvens.ptm");
-	montanha = PTMReader::read("Graphics/Montanhas.ptm");
-	plataforma2 = PTMReader::read("Graphics/Plataforma2.ptm");
-	plataforma = PTMReader::read("Graphics/Plataforma.ptm");
-	chao = PTMReader::read("Graphics/Chao.ptm");
+	clouds = PTMReader::read("Graphics/Nuvens.ptm");
+	mountain = PTMReader::read("Graphics/Montanhas.ptm");
+	backPlatform = PTMReader::read("Graphics/Plataforma2.ptm");
+	platform = PTMReader::read("Graphics/Plataforma.ptm");
+	ground = PTMReader::read("Graphics/Chao.ptm");
 
 	for(int i = 0; i < layers.size(); i++)
 		layers[i] = new Layer();
 
-	layers[0]->setImagem(nuvem);
-	layers[1]->setImagem(montanha);
-	layers[2]->setImagem(plataforma2);
-	layers[3]->setImagem(plataforma);
-	layers[4]->setImagem(chao);
+	layers[0]->setImagem(clouds);
+	layers[1]->setImagem(mountain);
+	layers[2]->setImagem(backPlatform);
+	layers[3]->setImagem(platform);
+	layers[4]->setImagem(ground);
 
 	float mainWidth = layers[4]->getImagem()->getWidth();
 
@@ -283,42 +238,60 @@ void initJogo(void) {
 }
 
 void initPersonagem(void) {
-	personagem = new GameObject();
-	personagem->init(PTMReader::read("Graphics/Personagem.ptm"));
-	personagem->setX(150);
-	personagem->setY(300);
-}
-
-void reshape(int width, int height) {
-	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(60, (GLfloat)width / (GLfloat)height, 1.0, 100.0);
-	glMatrixMode(GL_MODELVIEW);
+	player = new GameObject();
+	player->init(PTMReader::read("Graphics/Personagem.ptm"));
+	player->setX(150);
+	player->setY(300);
 }
 
 int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(640, 480);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Exercicio Parallax");
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Game 2D", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
 
 	initJogo();
 	initPersonagem();
 	sceneComposition();
-	update(0);
 
-	glutDisplayFunc(display);
-	glutReshapeFunc(reshape);
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window)) {
+		float ratio;
+		int width, height;
 
-	for (int i = 0; i < 255; i++)
-		keyStates[i] = false;
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
 
-	glutSetKeyRepeat(GLUT_KEY_REPEAT_OFF);
-	glutKeyboardFunc(keyPressed);
-	glutKeyboardUpFunc(keyUp);
+		glViewport(0, 0, width, height);
 
-	glutMainLoop();
-	return 0;
+		update();
+
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawPixels((*scene).getWidth(), (*scene).getHeight(), GL_BGRA_EXT, GL_UNSIGNED_BYTE, (*scene).getPixels());
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+	return EXIT_SUCCESS;
 }
